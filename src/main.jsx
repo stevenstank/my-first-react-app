c8afde3da21f4188c300e72cd97d37185f8c74bb
stevenstank <stevenstank775@gmail.com>
2026-03-05 12:05:17 +0000

    holi toh itni acchi gyi nhi

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Greet from './react.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Greet />
   </StrictMode>,
 )
---
+++ src/react.jsx
@@ -0,0 +1,5 @@
+function Greet(){
+    return <h1>HAPPY HOLI LAADLOOOOOO</h1>
+}
+
+export default Greet;
