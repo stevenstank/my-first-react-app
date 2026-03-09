bb8a55ef577d611aa80ef500d4660776f0741438
stevenstank <stevenstank775@gmail.com>
2026-03-09 15:31:56 +0000

    passed my first test

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+  });
+});
