c9b50a977243066ed7c0246094ebc83e6754b5cb
stevenstank <stevenstank775@gmail.com>
2026-03-10 20:09:58 +0000

    added some things (gonna remove them later)

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import App from "./App";
-import Profile from "./Profile";
-import DefaultProfile from "./DefaultProfile";
-import Spinach from "./Spinach";
-import Popeye from "./Popeye";
+import routes from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 );
